553e202b51c44cee65e39c0f2464a8faf59d5080
Marcin Filip <marcinosf@gmail.com>
2018-01-21 12:19:29 +0100

    Gulp - Browserify

--- server/main.js
+++ server/main.js
@@ -6,5 +6,5 @@ app.get('/', function(req,res) {
 	res.render('./../app/index.ejs', {});
 })
 
-.use(express.static(__dirname +  '/../app'))
+.use(express.static(__dirname +  '/../.tmp'))
 .listen(7777);
